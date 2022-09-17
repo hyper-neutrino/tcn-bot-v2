c76fcde5fd01694776c9e60689a72205da17b8f1
hyper-neutrino <alex.yj.liao@gmail.com>
2022-09-17 16:08:52 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,32 +8,35 @@ process.on("uncaughtException", (error) => {
     console.error(error?.stack ?? error);
 });
 
-client.on("ready", async () => {
-    setInterval(async () => {
-        for (const poll of await db("polls").find({}).toArray()) {
-            if (poll.time && !poll.closed && poll.time <= new Date()) {
-                let message;
+async function loop(client) {
+    for (const poll of await db("polls").find({}).toArray()) {
+        if (poll.time && !poll.closed && poll.time <= new Date()) {
+            let message;
 
-                try {
-                    const channel = await client.channels.fetch(poll.channel);
-                    message = await channel.messages.fetch(poll.message);
-                } catch (e) {
-                    console.error(e?.stack ?? e);
-                }
-
-                await client.log(
-                    `[AUTO] Closing the poll with ID \`${poll.id}\`.`
-                );
-                await close(message, poll);
-            } else if (poll.dm) {
-                const time = new Date();
-                time.setSeconds(time.getSeconds() + poll.dm * 3600);
-
-                if (time > poll.time) await dm(client, poll);
+            try {
+                const channel = await client.channels.fetch(poll.channel);
+                message = await channel.messages.fetch(poll.message);
+            } catch (e) {
+                console.error(e?.stack ?? e);
             }
-        }
-    }, 10000);
 
+            await client.log(
+                `[AUTO] Closing the poll with ID \`${poll.id}\`.`
+            );
+            await close(message, poll);
+        } else if (poll.dm) {
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + poll.dm * 3600);
+
+            if (time > poll.time) await dm(client, poll);
+        }
+    }
+    
+    setTimeout(() => loop(client), 10000);
+}
+
+client.on("ready", async () => {
+    loop(client);
     console.log("TCN bot is ready.");
 });
 
